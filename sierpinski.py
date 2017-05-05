224175f95fd5ada047ef8abe3ca28ae2c230d947
jakub.wojcik <jakubwojcik.post@gmail.com>
2017-05-05 19:27:30 +0200

    Lattice paths

--- sierpinski.py
+++ sierpinski.py
@@ -1,22 +1,23 @@
-# Lattice paths
 
 # Printing
 def print_square(s):
     for r in s:
         line_str = ""
         for value in r:
-            line_str += str(value)
+            if value == 1:
+                line_str += "o"
+            else:
+                line_str += " "
             line_str += " "
         print(line_str)
     return
 
 square = []
-n = 20
-n += 1
+n = 40
 
 for i in range(n):
     row = []
-    for j in range(n):
+    for j in range(n-i):
         row.append(0)
     square.append(row)
 
@@ -29,11 +30,13 @@ print_square(square)
 print("After injecting correct values:")
 for i in range(n):
     for j in range(n):
-        if i > 0:
-            square[i][j] += square[i-1][j]
-        if j > 0:
-            square[i][j] += square[i][j-1]
+        if j < len(square[i]):
+            if i > 0:
+                square[i][j] += square[i-1][j]
+            if j > 0:
+                square[i][j] += square[i][j-1]
+            square[i][j] = square[i][j]%2
 
 print_square(square)
 
-print("The answer is %d" % square[n-1][n-1])
+# print("The answer is %d" % square[n-1][n-1])
